57f630d71082dba371a4d3529ee57bf4841f88fc
Prethu-Raj-Debnath <prethuraj2002@gmail.com>
2026-02-07 19:52:48 +0600

    make backend capable serving frontend in production

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -12,5 +17,14 @@ const PORT = process.env.PORT || 3000;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+const frontendDistPath = path.resolve(__dirname, "..","..","frontend","dist");
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(frontendDistPath));
+
+  app.get("{*path}", (_req, res) => {
+    res.sendFile(path.join(frontendDistPath, "index.html"));
+  });
+}
 
 app.listen(PORT, () => console.log("Server running on port: " + PORT));
